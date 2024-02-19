12b43c80b0af21866b694b3a34850dc857515ab3
Usuario <Usuario@CPD03-M17>
2024-02-19 12:20:05 -0300

    Add codigo eq 2 grau

--- MeuPrimeiroMaui/MainPage.xaml.cs
+++ MeuPrimeiroMaui/MainPage.xaml.cs
@@ -11,7 +11,24 @@
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            
+            double a = Convert.ToDouble(txt_a.Text);
+            double b = Convert.ToDouble(txt_b.Text);
+            double c = Convert.ToDouble(txt_c.Text);
+
+            double delta = 0;
+
+            delta = (b * b) - 4 * a * c;
+
+            if (delta > 0)
+            {
+                double x1 = -b + Math.Sqrt(delta) / 2 * a;
+
+                double x2 = -b - Math.Sqrt(delta) / 2 * a;
+
+                string msg = $"O resultado é {x1} e {x2} ";
+
+                DisplayAlert("Resultado", msg, "Fechar");
+            }
 
         }
     }
